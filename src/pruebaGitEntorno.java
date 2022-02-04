2382b3b2ec28893babeb5ea3e771aa365cc7ce6e
CHRISTIAN <juancacaxzsc@gmail.com>
2022-02-04 16:18:14 +0100

    prueba3

--- src/pruebaGitEntorno.java
+++ src/pruebaGitEntorno.java
@@ -3,5 +3,7 @@ public class pruebaGitEntorno {
 	public static void main (String[]args) {
 		System.out.print("prueba de git");
 		System.out.println("prueba 2");
+		System.out.println("prueba 3");
+		
 	}
 }
